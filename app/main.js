2f7fdde58111f1cc9e1d37fa86684ce2d4557a43
TC <tcjcastillo@gmail.com>
2016-05-15 21:17:59 -0700

    Tutorial done

---
+++ app/main.js
@@ -0,0 +1,11 @@
+/**
+ * Loading the root component
+ *
+ * "main is where we tell Angular to launch the application.
+ * We also catch and log launch errors to the console."
+ */
+"use strict";
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var app_component_1 = require('./app.component');
+platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent); // platform specific, not from ng core
+//# sourceMappingURL=main.js.map
